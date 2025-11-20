71325d8160b52477aac387d873a4ac2e4da86d99
Maria Luiza <157306008+LuizaBorges23@users.noreply.github.com>
2025-11-20 16:17:40 -0300

    commit

--- Controle-de-gastos-main/app/src/main/java/com/example/controle_de_gastos/db/TransacaoDao.java
+++ Controle-de-gastos-main/app/src/main/java/com/example/controle_de_gastos/db/TransacaoDao.java
@@ -15,17 +15,15 @@ public interface TransacaoDao {
     @Query("SELECT * FROM transacoes ORDER BY data DESC")
     LiveData<List<Transacao>> getAllTransacoes();
 
-    // Query para calcular o saldo total (SEU CÓDIGO)
+  
     @Query("SELECT SUM(CASE WHEN tipo = 'Receita' THEN valor ELSE -valor END) FROM transacoes")
     LiveData<Double> getTotalBalance();
 
-    // --- NOVO ---
-    // Query para somar apenas as Receitas (Ganhos)
+    
     @Query("SELECT SUM(valor) FROM transacoes WHERE tipo = 'Receita'")
     LiveData<Double> getTotalIncome();
 
-    // --- NOVO ---
-    // Query para somar apenas as Despesas (Gastos)
+    
     @Query("SELECT SUM(valor) FROM transacoes WHERE tipo = 'Despesa'")
     LiveData<Double> getTotalExpenses();
 }

15812ddc717c92361fb0685b18b5994a04dd600f
Maria Luiza <157306008+LuizaBorges23@users.noreply.github.com>
2025-11-20 16:28:01 -0300

    Update Transacao

--- Controle-de-gastos-main/app/src/main/java/com/example/controle_de_gastos/repository/TransacaoRepositorio.java
+++ Controle-de-gastos-main/app/src/main/java/com/example/controle_de_gastos/repository/TransacaoRepositorio.java
@@ -5,14 +5,14 @@ import android.app.Application;
 import androidx.lifecycle.LiveData;
 
 import com.example.controle_de_gastos.db.AppDataBase;
-import com.example.controle_de_gastos.db.BudgetCategoryDao; // <-- IMPORT ADICIONADO
+import com.example.controle_de_gastos.db.OrcamentoCategiaDao; 
 import com.example.controle_de_gastos.db.TransacaoDao;
 import com.example.controle_de_gastos.model.Transacao;
 
 import java.util.List;
 public class TransacaoRepositorio {
     private TransacaoDao mTransacaoDao;
-    private BudgetCategoryDao mBudgetCategoryDao; // <-- ADICIONADO
+    private OrcamentoCategiaDao mOrcamentoCategiaDao; 
     private LiveData<List<Transacao>> mAllTransacoes;
     private LiveData<Double> mTotalBalance;
     private LiveData<Double> mTotalIncome;
@@ -21,7 +21,7 @@ public class TransacaoRepositorio {
     public TransacaoRepositorio(Application application) {
         AppDataBase db = AppDataBase.getDatabase(application);
         mTransacaoDao = db.transacaoDao();
-        mBudgetCategoryDao = db.budgetCategoryDao(); // <-- ADICIONADO
+        mBudgetCategoryDao = db.budgetCategoryDao(); 
 
         mAllTransacoes = mTransacaoDao.getAllTransacoes();
         mTotalBalance = mTransacaoDao.getTotalBalance();
@@ -45,25 +45,24 @@ public class TransacaoRepositorio {
         return mTotalExpenses;
     }
 
-    // --- MÉTODO INSERT MODIFICADO ---
+    
     public void insert(Transacao transacao) {
         AppDataBase.databaseWriteExecutor.execute(() -> {
 
-            // 1. Insere a transação (como antes)
+            
             mTransacaoDao.insert(transacao);
 
-            // 2. Lógica para atualizar o "Cofrinho" (Finanças)
-            // Se for uma "Despesa", atualiza o cofrinho correspondente
+            
             if ("Despesa".equals(transacao.getTipo())) {
-                // O valor da transação (ex: 50.00)
+                
                 double valorDespesa = transacao.getValor();
-                // A categoria da transação (ex: "Lazer")
+                
                 String categoria = transacao.getCategoria();
 
-                // Manda o DAO subtrair o valor do cofrinho "Lazer"
-                mBudgetCategoryDao.updateBudgetOnDespesa(categoria, valorDespesa);
+                
+                mOrcamentoCategiaDao.updateBudgetOnDespesa(categoria, valorDespesa);
             }
-            // (Nota: Se for "Receita", não fazemos nada com os cofrinhos por enquanto)
+            
         });
     }
 }

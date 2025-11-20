a309fd1e22cd2344649d7df288818b0cbb2bd038
Maria Luiza <157306008+LuizaBorges23@users.noreply.github.com>
2025-11-20 16:31:10 -0300

    Update AddRecurringPaymentActivity

--- Controle-de-gastos-main/app/src/main/java/com/example/controle_de_gastos/view/AddRecurringPaymentActivity.java
+++ Controle-de-gastos-main/app/src/main/java/com/example/controle_de_gastos/view/AddRecurringPaymentActivity.java
@@ -29,7 +29,7 @@ public class AddRecurringPaymentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recurring_payment);
 
-        // Encontrar os componentes
+        
         editTextNome = findViewById(R.id.edit_text_payment_nome);
         editTextCategoria = findViewById(R.id.edit_text_payment_categoria);
         editTextValor = findViewById(R.id.edit_text_payment_valor);
@@ -37,23 +37,23 @@ public class AddRecurringPaymentActivity extends AppCompatActivity {
         buttonSave = findViewById(R.id.button_save_recurring_payment);
         toolbar = findViewById(R.id.toolbar_add_recurring);
 
-        // Configurar o ViewModel
+      
         viewModel = new ViewModelProvider(this).get(RecurringPaymentViewModel.class);
 
-        // Configurar o botão de voltar da Toolbar
+        
         toolbar.setNavigationOnClickListener(v -> finish());
 
-        // Configurar o clique do botão Salvar
+        
         buttonSave.setOnClickListener(v -> savePayment());
     }
 
-    private void savePayment() {
+    private void savarPagamento() {
         String nome = editTextNome.getText().toString().trim();
         String categoria = editTextCategoria.getText().toString().trim();
         String valorStr = editTextValor.getText().toString().trim();
         String diaStr = editTextDia.getText().toString().trim();
 
-        // Validação simples
+        
         if (nome.isEmpty() || categoria.isEmpty() || valorStr.isEmpty() || diaStr.isEmpty()) {
             Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
             return;
@@ -63,18 +63,18 @@ public class AddRecurringPaymentActivity extends AppCompatActivity {
             double valor = Double.parseDouble(valorStr.replace(",", "."));
             int dia = Integer.parseInt(diaStr);
 
-            // Validação do dia
+            
             if (dia < 1 || dia > 31) {
                 Toast.makeText(this, "Dia do vencimento inválido (1-31)", Toast.LENGTH_SHORT).show();
                 return;
             }
 
-            // Criar e salvar o objeto
+            
             RecurringPayment payment = new RecurringPayment(nome, categoria, valor, dia);
             viewModel.insert(payment);
 
             Toast.makeText(this, "Pagamento Fixo salvo!", Toast.LENGTH_SHORT).show();
-            finish(); // Fecha a tela e volta para a MainActivity
+            finish(); 
 
         } catch (NumberFormatException e) {
             Toast.makeText(this, "Valor ou Dia inválido", Toast.LENGTH_SHORT).show();
